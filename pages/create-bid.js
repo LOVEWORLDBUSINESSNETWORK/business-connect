ff3498c095d5ba4203c4a5cae5e2e0d3422242dc
charayo <69734231+charayo@users.noreply.github.com>
2021-06-14 10:17:45 +0000

    create-bid

--- pages/create-bid.js
+++ pages/create-bid.js
@@ -36,9 +36,8 @@ class CreateBid extends Component {
                 <div className="right-side mr-5">
                   <span className="">Time</span>
                   <div className="mt-3">
-                    <span className="left-rad bg-light">AM</span>
-                    <span className=" no-rad bg-light">PM</span>
-                    <span className="no-rad bg-light">MM</span>
+                    <span className="left-rad bg-light pointer">AM</span>
+                    <span className=" no-rad bg-light pointer">PM</span>
                     <span className="right-rad bg-light time-sel">
                       <input type="time" className="border-0" />
                     </span>
@@ -46,24 +45,33 @@ class CreateBid extends Component {
                 </div>
               </div>
 
-              <div className="mt-3">
-                <h6 className="">Whats's your budget</h6>
-                <div className="bid-curr mt-3">
-                    <select className="form-control w-25 full-rad">
-                        <option  value="">select currency</option>
-                    </select>
-                    <input type="text" className="form-control w-25 full-rad budget" placeholder="Min Budget"/>
-                    <input type="text" className="form-control w-25 full-rad budget" placeholder="Max Budget"/>
+                <div className="mt-3">
+                    <h6 className="">Whats's your budget</h6>
+                    <div className="bid-curr mt-3">
+                        <select className="form-control w-25 full-rad">
+                            <option  value="">select currency</option>
+                        </select>
+                        <input type="text" className="form-control w-25 full-rad budget" placeholder="Min Budget"/>
+                        <input type="text" className="form-control w-25 full-rad budget" placeholder="Max Budget"/>
+                    </div>
+                    <div className="">
+                        <textarea className="form-control mt-4 my-text-box"></textarea>
+                    </div>
                 </div>
-                <div className="">
-                    <textarea className="form-control mt-4 my-text-box"></textarea>
+                <div className="mt-4">
+                    <h6>Upload document if necessary</h6>
+                    <div className="file-uploads my-4">
+                    <div className="">
+                        <label className='file-label' for ="files">Upload Document &uarr;</label>
+                        <input id="files" type="file" className="form-control w-25 custom-file" />
+                    </div>
+                    <div className="drag-n-drop">
+                        <span >Drag and Drop </span><strong>&#43;</strong>
+                    </div>
                 </div>
-              </div>
-              <div>
-                  <h6>Upload document if necessary</h6>
-                  <div className="">
-                      <input type="file" className="form-control w-25" />
-                  </div>
+                <div className="text-center">
+                    <input type="submit" className="btn btn-lbn" value="Submit"/>
+                </div>
               </div>
             </div>
           </div>
